120a8b09cf3be39e05d93c4d2648cac88bdad9ad
Yago dos Santos <108253694+Santos-Yago@users.noreply.github.com>
2023-05-30 18:40:58 -0300

    Update Mundo.cpp

--- Mundo.cpp
+++ Mundo.cpp
@@ -4,8 +4,8 @@ Mundo::Mundo(sf::RenderWindow& janela) :m_janela{ janela },
 m_visaoMundo{ janela.getDefaultView() },
 m_texturas{}, m_cenaGraficos{}, m_rotCenas{},
 m_mundoBordas{
-	0.f,						// esquerda, posição X
-	0.f,						// cima, posição Y
+	0.f,						// esquerda, posiÃ§Ã£o X
+	0.f,						// cima, posiÃ§Ã£o Y
 	m_visaoMundo.getSize().x,	// Largura;
 	2000.f						// Altura;
 }, m_posicaoSpawn{
@@ -23,7 +23,7 @@ void Mundo::m_carregarTexturas()
 {
 	m_texturas.carregar(Texturas::ID::Eagle, "E:/Texturas/Eagle.png");
 	m_texturas.carregar(Texturas::ID::Raptor, "E:/Texturas/Raptor.png");
-	m_texturas.carregar(Texturas::ID::Deserto, "E:/Texturas/Desert.png");
+	m_texturas.carregar(Texturas::ID::Desert, "E:/Texturas/Desert.png");
 }
 
 void Mundo::m_criarCena()
@@ -36,9 +36,9 @@ void Mundo::m_criarCena()
 		m_cenaGraficos.alocarSecundario(std::move(rotulo));
 	}
 
-	//Configura a repetição do sprite de fundo. Cria uma textura retangular usando
-	//a conversão recebida de 'm_mundoBordas'
-	sf::Texture& textura{m_texturas.obter(Texturas::ID::Deserto)};
+	//Configura a repetiÃ§Ã£o do sprite de fundo. Cria uma textura retangular usando
+	//a conversÃ£o recebida de 'm_mundoBordas'
+	sf::Texture& textura{m_texturas.obter(Texturas::ID::Desert)};
 	sf::IntRect texRetangulo{m_mundoBordas};
 	textura.setRepeated(true);
 
@@ -54,31 +54,31 @@ void Mundo::m_criarCena()
 	m_naveJogador->configVelocidade(50.f, m_velocidadeRolagem);
 	m_rotCenas[Ar]->alocarSecundario(std::move(lider));
 
-	//Escoltas do líder - Esquerda
+	//Escoltas do lÃ­der - Esquerda
 	std::unique_ptr<Aeronave> escoltaEsquerda{new Aeronave(Aeronave::Tipo::Raptor, m_texturas)};
 	escoltaEsquerda->setPosition(-80.f, 50.f);
 	m_naveJogador->alocarSecundario(std::move(escoltaEsquerda));
 
-	//Escolta do líder - Direita
+	//Escolta do lÃ­der - Direita
 	std::unique_ptr<Aeronave> escoltaDireita{new Aeronave(Aeronave::Tipo::Raptor, m_texturas)};
 	escoltaDireita->setPosition(80.f, 50.f);
 	m_naveJogador->alocarSecundario(std::move(escoltaDireita));
 }
 
-//Desenha as configurações na janela em tempo real e encarrega o trabalho para SceneGraph
+//Desenha as configuraÃ§Ãµes na janela em tempo real e encarrega o trabalho para SceneGraph
 void Mundo::desenhar()
 {
 	m_janela.setView(m_visaoMundo);
 	m_janela.draw(m_cenaGraficos);
 }
 
-//A função abaixo controla a rolagem do mundo e sua movimentação
+//A funÃ§Ã£o abaixo controla a rolagem do mundo e sua movimentaÃ§Ã£o
 void Mundo::atualizar(sf::Time deltaTempo)
 {
-	//A visão é rolada de acordo com o tempo passado
+	//A visÃ£o Ã© rolada de acordo com o tempo passado
 	m_visaoMundo.move(0.f, m_velocidadeRolagem * deltaTempo.asSeconds());
 
-	//Checamos a aproximação da borda
+	//Checamos a aproximaÃ§Ã£o da borda
 	sf::Vector2f posicionamento{m_naveJogador->obterPontoDoMundo()};
 	sf::Vector2f velocidade{m_naveJogador->obterVelocidade()};
 	if (posicionamento.x <= m_mundoBordas.left + 150.f ||
